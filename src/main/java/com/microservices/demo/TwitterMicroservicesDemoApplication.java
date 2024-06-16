0726680ff248393c6d9146a4a22ce142450d21c5
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2024-06-16 16:40:57 +0800

    Initial Commit

---
+++ src/main/java/com/microservices/demo/TwitterMicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMicroservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMicroservicesDemoApplication.class, args);
+	}
+
+}
