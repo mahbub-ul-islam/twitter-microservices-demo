a9b0d5681350f26cdf8dd1c20a05fe2fe0779691
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2024-06-20 21:33:00 +0800

    Boot up twitter-to-kafka-service module. Closes #4

--- twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/TwitterToKafkaServiceApplication.java
@@ -1,8 +1,13 @@
 package com.microservices;
 
+import com.microservices.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 /**
  * Created by mahbub.islam on 20/6/2024.
@@ -10,12 +15,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class);
     }
 
     @Override
-    public void run(String... args) {
-        System.out.println("App Starts...");
+    public void run(String... args) throws Exception {
+        LOG.info("App Starts...");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray()));
     }
 }
---
+++ twitter-to-kafka-service/src/test/java/com/microservices/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,18 @@
+package com.microservices;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by mahbub.islam on 20/6/2024.
+ */
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
